7005e10e9b43e9fdebb4f70b53f3a826038e7287
Leo <okrieg@t-online.de>
2020-05-15 19:47:19 +0200

    modal completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,10 @@
 import '../styles/styles.css'
-import MobileMenu from './modules/MobelMenu'
+import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
+import Modal from './modules/Modal'
 
+new Modal()
 let stickyHeader = new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
@@ -10,6 +12,5 @@ new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 let mobileMenu = new MobileMenu();
 
 if (module.hot) {
-    module.hot.accept()
+  module.hot.accept()
 }
-
